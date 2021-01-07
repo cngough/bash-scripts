96759ee03fe37491cd0b50267bbd146b49bf6d9e
Chris Gough <13353626+cngough@users.noreply.github.com>
2021-01-07 11:25:43 +0000

    Added first pass to pull all git projects in a folder

---
+++ pull-all-projects/pull-all-projects.sh
@@ -0,0 +1,5 @@
+# -I{} execThis.sh {} replaces {} with the full string - e.g. 
+# foo.bar -> execThis.sh foo.bar
+# git -C does the same as git --git-dir (uses an OS-level directory chdir operation), with the distinction that $GIT_DIR is set to a relative path. This changes the way sub-commands locate the repository
+# Task - do a check to see if we're on develop
+find . -name ".git" -type d | sed 's/\/.git//' | xargs -P10 -I{} git -C {} pull
