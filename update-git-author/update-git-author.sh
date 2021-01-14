017de43ca5cf5487863916712dcdb4fda4f4360f
Chris Gough <13353626+cngough@users.noreply.github.com>
2021-01-14 07:00:34 +0000

    Commit to try on another box

--- update-git-author/update-git-author.sh
+++ update-git-author/update-git-author.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function usage {
-  echo "Usage: $(basename $0) -e [old email] [new email] -n [old name] [new name] -f" 2>&1
+  echo "Usage: $(basename $0) -e [old email;new email] -n [old name;new name] -f" 2>&1
+  echo "Example: update-git-author.sh -e \"john@foo.com;john@bar.com\" -n \"jsmith;John Smith\""
   echo '  -e    replaces an old author email address with the provided one'
   echo '  -n    replaces an old author name with the provided one'
-  echo '  -f    forces an update'
+  echo '  -f    forces Git to force filter-branch'
   exit 1
 }
 
@@ -16,9 +17,10 @@ options=":enf"
 
 while getopts ${options} arg; do
   case "${arg}" in
-    e) echo "Option 'e' was called with ${arg}" ;;
-    n) echo "Option 'n' was called with ${arg}" ;;
-
+    e)
+       echo "argument -e called with parameter $OPTARG" >&2;;
+    n) 
+      echo "N VALUE: ${OPTARG}";;
     ?)
       echo "Invalid option: -${OPTARG}."
       echo
@@ -27,21 +29,21 @@ while getopts ${options} arg; do
   esac
 done
 
-echo "Replacing commits by '$1' with '$2', '$3'"
-echo "Continue? [y/n]"
-read CONFIRM
+# echo "Replacing commits by '$1' with '$2', '$3'"
+# echo "Continue? [y/n]"
+# read CONFIRM
 
-if [[ ! $CONFIRM =~ ^[Yy]$ ]]
-  then
-    exit 1
-fi
+# if [[ ! $CONFIRM =~ ^[Yy]$ ]]
+#   then
+#     exit 1
+# fi
 
-git filter-branch --commit-filter '
-        if [ "$GIT_AUTHOR_EMAIL" = "$1" ];
-        then
-                GIT_AUTHOR_NAME="$2";
-                GIT_AUTHOR_EMAIL="$3";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi' HEAD
+# git filter-branch --commit-filter '
+#         if [ "$GIT_AUTHOR_EMAIL" = "$1" ];
+#         then
+#                 GIT_AUTHOR_NAME="$2";
+#                 GIT_AUTHOR_EMAIL="$3";
+#                 git commit-tree "$@";
+#         else
+#                 git commit-tree "$@";
+#         fi' HEAD
