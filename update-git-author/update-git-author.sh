86374c00b2c4dc63ffdaa92308b69fa933c4c4b5
Chris Gough <13353626+cngough@users.noreply.github.com>
2021-01-14 06:10:54 +0000

    Using getopts to parse arguments

--- update-git-author/update-git-author.sh
+++ update-git-author/update-git-author.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]
-  then
-    echo "No arguments supplied. Usage is git-fix.sh <OLD_AUTHOR_EMAIL> <NEW_AUTHOR_EMAIL> <NEW AUTHOR_EMAIL>"
-    exit 1
+function usage {
+  echo "Usage: $(basename $0) -e [old email] [new email] -n [old name] [new name] -f" 2>&1
+  echo '  -e    replaces an old author email address with the provided one'
+  echo '  -n    replaces an old author name with the provided one'
+  echo '  -f    forces an update'
+  exit 1
+}
+
+if [[ ${#} -eq 0 ]]; then
+   usage
 fi
+
+options=":enf"
+
+while getopts ${options} arg; do
+  case "${arg}" in
+    e) echo "Option 'e' was called" ;;
+    n) echo "Option 'n' was called" ;;
+
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      echo
+      usage
+      ;;
+  esac
+done
 
 echo "Replacing commits by '$1' with '$2', '$3'"
 echo "Continue? [y/n]"
@@ -12,7 +33,6 @@ read CONFIRM
 
 if [[ ! $CONFIRM =~ ^[Yy]$ ]]
   then
-    echo "Exiting without modification"
     exit 1
 fi
 
