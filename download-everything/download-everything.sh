2a21d4e8cc76aa2977650eec63ba37fa58640dc8
Chris Gough <13353626+cngough@users.noreply.github.com>
2021-07-20 18:39:18 +0100

    Create download-everything.sh

---
+++ download-everything/download-everything.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#title           :download-everything.sh
+#description     :Given a URL it will download absolutely everything and put it in nice nested directories
+#author			 :chris@chris-gough.com
+#date            :20181015
+#version         :0.1
+#==============================================================================
+
+# Basic sanitisation - null $1
+if [ -z "$1" ] ; then 
+	echo "Usage is download-everything.sh [url-of-resources]"
+	exit 1
+fi
+
+# Again we're doing this insecurely (--no-check-certificate) for now
+wget --no-check-certificate --mirror -p --convert-links -P ./ $1
