aede33db59805dcb7eb7612a8b3abf97da33ac12
Chris Gough <13353626+cngough@users.noreply.github.com>
2021-07-20 18:54:16 +0100

    Update and rename kill-docker-images/kill-docker-images.sh to kill-docker-containers/kill-docker-containers.sh

---
+++ kill-docker-containers/kill-docker-containers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function usage {
+  echo "Usage: $(basename $0) -c [container]" 2>&1
+  echo "Example: kill-docker-containers.sh -c container"
+  echo '  -i    the container name to be killed'
+  exit 1
+}
+
+options=":c:"
+
+while getopts ${options} arg; do
+  case "${arg}" in
+    c)
+       container="${OPTARG}"
+       ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      echo
+      usage
+      ;;
+  esac
+done
+
+if [[ $container ]]; then
+  echo "Killing all container with name:" $container
+  docker ps | grep $container | awk '{ print $1;}' | xargs docker kill
+fi
