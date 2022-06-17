a3e15de69e6a9fc8e60829694ca1db2635e7b2c5
Chris Gough <chris.gough@oodlefinance.com>
2022-06-17 12:00:46 +0100

    Added alternative command to prune-remote-branches

--- prune-remote-branches/prune-remote-branches.sh
+++ prune-remote-branches/prune-remote-branches.sh
@@ -3,3 +3,6 @@
 // if yes, perform same with -D
 
 git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
+
+// also:
+// git branch -r --merged | egrep -v "(^\*|master|dev)" | sed 's/origin\///' | xargs -n 1 git push origin --delete
